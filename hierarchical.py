0b80ca82dfd1735048d660b40a34d407dd1d1281
gusnunes <daniloaugusto_nunes@hotmail.com>
2020-06-04 02:35:04 -0300

    Finalizada funcao que calcula matriz de distancias

--- hierarchical.py
+++ hierarchical.py
@@ -8,9 +8,45 @@ def inicial_groups(data):
 
     return groups,size
 
+def distance_matrix(medias,size):   # procurar o menor valor na matriz de distancia
+    menor  = abs(medias[1] - medias[0])   # primeira distancia da matriz
+    row    = 1
+    column = 0
+
+    for i in range(2,size):
+        for j in range(size):
+            if i >= j:   # diagonal principal e acima(distancias repetidas e nulas)
+                break
+            
+            distancia = abs(medias[i] - medias[j])
+            if distancia < menor:
+                row    = i
+                column = j
+    
+    return row,column
+
 def hierarchical(data):
     groups,size = inicial_groups(data)
+    
+    if size < 2:   # erro na funcao que calcula a matriz de distancia
+        return groups
+
+    medias = data   # a media inicial sao os dados iniciais
     print(f"\nGrupos iniciais: {groups}\n")
+
+    cont = 1
+    while cont < size:
+        i,j = distance_matrix(medias,size)
+        if i < j:
+            groups[i] = groups[i] + groups[j]
+            groups.remove(groups[j])
+        else:
+            groups[j] = groups[j] + groups[i]
+            groups.remove(groups[i])
+        
+        print(f"Agrupamento {cont}: {groups}")
+
+        cont += 1
 
     return groups
 
